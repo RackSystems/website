7fc666e134eef717096b647f8b865632eb0b5978
iMatheusPaula <imatheuspaula@gmail.com>
2025-04-04 19:58:16 -0300

    tailwind :)

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <img src="/images/logo.png" alt="Logo Rack Systems" class="logo"/>
-  <div class="message">
-    <p>Transformamos seu negócio em um clique, seu catálogo online e seus clientes mais perto.</p>
-    <p>O site que sua loja precisa, com pedidos direto no WhatsApp!</p>
-    <a href="https://wa.me/+5567993030625" target="_blank">Entre em contato</a>
+  <img src="/images/logo.png" class="absolute top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+       alt="Logo Rack Systems"
+  />
+
+  <div class="absolute top-[calc(50%+80px)] left-1/2 transform -translate-x-1/2 text-center font-['Press_Start_2P']
+              text-[14px] text-fuchsia-500 max-w-[80%] [text-shadow:2px_2px_10px_rgba(255,0,255,0.8)]"
+  >
+    <p>
+      Transformamos seu negócio em um clique, seu catálogo online e seus clientes mais perto.
+    </p>
+    <p>
+      O site que sua loja precisa, com pedidos direto no WhatsApp!
+    </p>
+    <a href="https://wa.me/+5567993030625" target="_blank"
+       class="inline-block mt-4 px-5 py-2.5 text-[12px] text-cyan-400 border-2 border-cyan-400 rounded
+       shadow-[0_0_10px_rgba(0,255,255,0.8)] transition-all duration-300 hover:bg-cyan-400 hover:text-black
+       hover:shadow-[0_0_20px_rgba(0,255,255,1)]"
+    >
+      Entre em contato
+    </a>
   </div>
 
   <div class="bg"></div>
