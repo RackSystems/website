c6396f9e0da227dada5096acaa2477fdf6654d3f
iMatheusPaula <imatheuspaula@gmail.com>
2025-04-05 16:40:36 -0300

    add ecosystem configuration for development environment

---
+++  ecosystem.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: 'racksystems-dev',
+            port: '3000',
+            script: 'npm',
+            args: 'run dev',
+        }
+    ]
+}
