3b14686cd9b2f34eb62b9a8727b3da88ec06c848
iMatheusPaula <imatheuspaula@gmail.com>
2025-04-05 23:34:16 -0300

    add Vite server configuration to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/ui'
-  ],
-  css: [
-    '@/assets/css/main.css'
-  ],
-  app: {
-    head: {
-        title: 'Rack Systems - Software House',
-        meta: [
-            { name: 'description', content: 'Rack Systems - Transformamos seu negócio em um clique, seu catálogo online e seus clientes mais perto.' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ]
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
+    modules: [
+        '@nuxt/eslint',
+        '@nuxt/fonts',
+        '@nuxt/icon',
+        '@nuxt/ui'
+    ],
+    css: [
+        '@/assets/css/main.css'
+    ],
+    app: {
+        head: {
+            title: 'Rack Systems - Software House',
+            meta: [
+                {
+                    name: 'description',
+                    content: 'Rack Systems - Transformamos seu negócio em um clique, seu catálogo online e seus clientes mais perto.'
+                },
+                {name: 'viewport', content: 'width=device-width, initial-scale=1'}
+            ],
+            link: [
+                {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+            ]
+        }
+    },
+    vite: {
+        server: {
+            allowedHosts: ['dev.racksystems.com.br']
+        }
     }
-  }
 })
